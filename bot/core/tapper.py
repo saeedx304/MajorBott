2e91fb67b04393f1c74b738169c82b12a0fab487
Gerfesto <hameilleb@gmail.com>
2024-08-29 18:55:43 +0300

    headers update

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -243,6 +243,7 @@ class Tapper:
                     http_client = aiohttp.ClientSession(headers=headers, connector=proxy_conn)
                     if settings.FAKE_USERAGENT:            
                         http_client.headers['User-Agent'] = generate_random_user_agent(device_type='android', browser_type='chrome')
+                
                 user_data = await self.login(http_client=http_client, init_data=init_data, ref_id=ref_id)
                 if not user_data:
                     logger.info(f"{self.session_name} | <r>Failed login</r>")
